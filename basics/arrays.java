0103c8209bb27a7a8c78d02a99400a4562657d47
jessicabrower037 <121197501+jessicabrower037@users.noreply.github.com>
2024-01-25 18:45:17 -0500

    Delete basics directory

--- basics/arrays.java
+++
@@ -1,9 +0,0 @@
-package basics;
-
-public class arrays {
-    public static void main(String [] args){
-        String[] vehicles = {"Toyota","Ford","GM","Volvo"};
-        System.out.println(vehicles[0]);
-        System.out.println(vehicles[3]);
-    }
-}
